5bc1ac16d654ab4048447f24048cff47eda78ef5
chang-ning <changning.tsai@sifive.com>
2022-06-06 16:24:09 -0700

    remove unused jenkins data
    Signed-off-by: chang-ning <changning.tsai@sifive.com>

---
+++ jenkins/groovy/doQuietDown.groovy
@@ -0,0 +1,33 @@
+import jenkins.model.Jenkins;
+import hudson.model.*
+
+// start in the state that doesn't do any build.
+def q = Jenkins.instance.queue
+for (queued in Jenkins.instance.queue.items) {
+  q.cancel(queued.task)
+}
+// stop all the currently running jobs
+for (job in Jenkins.instance.items) {
+  stopJobs(job)
+}
+
+def stopJobs(job) {
+  if (job in com.cloudbees.hudson.plugins.folder.Folder) {
+    for (child in job.items) {
+      stopJobs(child)
+    }
+  } else if (job in org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject) {
+    for (child in job.items) {
+      stopJobs(child)
+    }
+  } else if (job in org.jenkinsci.plugins.workflow.job.WorkflowJob) {
+    if (job.isBuilding()) {
+      for (build in job.builds) {
+      	build.doKill()
+      }
+    }
+    job.disabled = true
+  }
+}
+
+Jenkins.instance.doQuietDown();
